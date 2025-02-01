d42ad39da9dbf662d0540deb8fbe5e27aba891fe
keerthika290 <keerthikasri2904@gmail.com>
2025-02-01 14:50:46 +0530

    my first commit

--- asset/js/main.js
+++ asset/js/main.js
@@ -43,3 +43,24 @@ $('.response-slide').slick({
     navMenu.classList.toggle("active");
     nav.classList.toggle("nav-active");
   });
+
+  // counter //
+ var a = 0;
+ $(window).scroll(function () {
+ var oTop = $('#counter').offset().top - window.innerHeight;
+ if (a == 0 && $(window).scrollTop() > oTop) {
+     $('.counter').each(function () {
+     var $this = $(this);
+     jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+         duration: 4000,
+         easing: 'swing',
+         step: function () {
+         $this.text(Math.ceil(this.Counter));
+         }
+     });
+     });
+     a = 1;
+ }
+ });
+
+
